5d2f864bf4065b001b60934827447ac5be79b91e
jaisminjata <jaisminjata195@gmail.com>
2023-12-17 15:11:56 +0530

    added feign and naming server

--- currency-conversion-service/src/main/java/com/microservice/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/microservice/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -1,9 +1,10 @@
 package com.microservice.currencyconversionservice;
-
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
